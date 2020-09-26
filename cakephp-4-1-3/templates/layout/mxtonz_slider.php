e08001a56ef2ce4167abdaca2b071242e79d8e8a
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2020-09-26 13:00:13 +0200

    end of refactor

--- cakephp-4-1-3/templates/layout/mxtonz_slider.php
+++ cakephp-4-1-3/templates/layout/mxtonz_slider.php
@@ -36,7 +36,7 @@ use Cake\Core\Configure;
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">     	
                  	<img src="/images/logo.png" alt="Logo" >
-                	<span class="h5 text-bg">Powiadomienia</span>
+                	<span class="h5">Powiadomienia</span>
                 </a>
                 <button class="navbar-toggler navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="icon-bar"></span>
