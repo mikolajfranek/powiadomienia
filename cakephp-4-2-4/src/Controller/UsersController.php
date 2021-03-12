25d0ecac5831ecf4414de6a3183c95500ca6f84c
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2021-03-12 23:05:06 +0100

    save

--- cakephp-4-2-4/src/Controller/UsersController.php
+++ cakephp-4-2-4/src/Controller/UsersController.php
@@ -7,6 +7,14 @@ use App\Form\RegisterForm;
 
 class UsersController extends AppController
 {
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        // Configure the login action to not require authentication, preventing
+        // the infinite redirect loop issue
+        $this->Authentication->addUnauthenticatedActions(['login', 'register']);
+    }
+    
     public function login(){
         $form = new LoginForm();
         $this->set('form', $form);
@@ -14,6 +22,8 @@ class UsersController extends AppController
     
     public function register(){
         $form = new RegisterForm();
+        
+        
         
         $this->set('form', $form);
     }
