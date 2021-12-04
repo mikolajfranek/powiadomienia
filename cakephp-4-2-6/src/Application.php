0448c7b1488ac9d54974713daa38d97abda47354
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2021-12-04 12:59:21 +0100

    require https

--- cakephp-4-2-6/src/Application.php
+++ cakephp-4-2-6/src/Application.php
@@ -35,6 +35,7 @@ use Authentication\Middleware\AuthenticationMiddleware;
 use Cake\Http\MiddlewareQueue;
 use Cake\Routing\Router;
 use Psr\Http\Message\ServerRequestInterface;
+use Cake\Http\Middleware\HttpsEnforcerMiddleware;
 
 /**
  * Application setup class.
@@ -106,7 +107,12 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
             // Parse various types of encoded request bodies so that they are
             // available as array through $request->getData()
             // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
-            ->add(new BodyParserMiddleware());
+            ->add(new BodyParserMiddleware())
+            ->add(new HttpsEnforcerMiddleware([
+                'disableOnDebug' => true,
+                'redirect' => true,
+                'statusCode' => 302,
+            ]));
 
         // Cross Site Request Forgery (CSRF) Protection Middleware
         // https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
